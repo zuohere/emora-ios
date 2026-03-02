5d16f55557bf84e3635cb73d9ccf53d2f990c7f2
zoe <zuohere.ayi@finnox.cn>
2026-03-02 15:28:33 +0800

    Initial Commit

---
+++ emora-ios/emora_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  emora_iosApp.swift
+//  emora-ios
+//
+//  Created by zora on 2026/3/2.
+//
+
+import SwiftUI
+
+@main
+struct emora_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
